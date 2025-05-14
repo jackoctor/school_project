00bd0ca15c040cd809717d7251a852b441ac8701
jackoctor <ryou.net.1024@gmail.com>
2025-05-14 19:22:22 +0900

    Update
    起動確認処理を追加

--- pi.c
+++ pi.c
@@ -4,16 +4,30 @@
 #define SENSOR_PIN //GPIOピンの番号を入れる
 #define LIGHT_PIN //GPIOピンの番号を入れる
 
+//起動確認処理
+void check(int times) {
+    delay(1000);
+    for (int i = 0; i < 3; i++){
+        digitalWrite(LIGHT_PIN, HIGH);
+        delay(500);
+        digitalWrite(LIGHT_PIN, LOW);
+        delay(500);
+    }
+    printf("Ready\n");
+}
+
 int main(void) {
     wiringPiSetupGpio ();
     pinMode (SENSOR_PIN, INPUT);
     pinMode (LIGHT_PIN, OUTPUT);
 
+    check(1);
+
     while (1) {
         if (digitalRead(SENSOR_PIN) == HIGH) {
             digitalWrite(LIGHT_PIN, HIGH); //ライト点灯
         } else {
-            digitalRead(LIGHT_PIN, LOW); //ライト消灯
+            digitalWrite(LIGHT_PIN, LOW); //ライト消灯
         }
         delay(100);
     }
